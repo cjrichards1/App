fa0888a252760dabc3609b17c47f11656234f5fe
cjrichards1 <richardsc42@cardiff.ac.uk>
2025-07-15 17:19:50 +0100

    Fix FlashcardForm props in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FlashcardForm as App } from './App.tsx'
+import { useFlashcards } from './hooks/useFlashcards'
 import './index.css'
+
+function AppWrapper() {
+  const { folders, categories, addFlashcard } = useFlashcards();
+  
+  return (
+    <App 
+      folders={folders}
+      categories={categories}
+      onAdd={addFlashcard}
+    />
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
 )
